5117dec947f7c1ca2a959e400cefd7c8d29b0795
Abhishek Gupta <shabhishekgupta890@gmail.com>
2025-08-08 16:03:18 +0530

    updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from '../components/NavBar'
+import Navbar from '../components/NavBar'
 import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
 import Testimonial from '../components/Testimonial'
